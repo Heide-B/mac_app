2be86fa9f95e48cccfa47b16c78a00f89ff2a9d7
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2023-11-26 11:36:23 +0800

    Doctor view: added basic progress

--- careworld/app/page_views/doctor_view.py
+++ careworld/app/page_views/doctor_view.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from utils.helpers import customize_widget
+from datetime import datetime
 
 def view(username: str, authenticator):
     with st.container():
@@ -8,5 +10,13 @@ def view(username: str, authenticator):
     with st.container():
         with st.expander("Patient Briggs"):
             st.title("Current Annua Progres")
-            st.progress(50)
+            st.progress(50,
+            "Patient Briggs has completed 50% of her consulations and laboratory exam")
+
+            tasks, status, next_due = st.columns([0.6, 0.2, 0.2])
+            tasks.write("Consultation")
+            status.warn("Upcoming")
+            next_due.date_input(help="Upcoming Schedule of consult. You can change /reschedule this task")
+            st.divider()
+        customize_widget("stExpander", "whit")
 
