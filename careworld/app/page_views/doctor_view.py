17c43feb79e353abee507a279a20513ed3028e2e
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2023-11-26 11:25:48 +0800

    Doctor view

--- careworld/app/page_views/doctor_view.py
+++ careworld/app/page_views/doctor_view.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
-def view(username: str):
-    st.write(f'Welcome doctor: {username}!')
+def view(username: str, authenticator):
+    with st.container():
+        st.write(f'Welcome doctor: {username}!')
+        st.divider()
+    
+    with st.container():
+        with st.expander("Patient Briggs"):
+            st.title("Current Annua Progres")
+            st.progress(50)
 
